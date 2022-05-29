b9f412ddf070a66029f862d3c2704a2aa8bea67a
Xing Voong <xingvoong@gmail.com>
2022-05-28 23:25:17 -0700

    init two sum problem

---
+++ two_sum/two_sum.py
@@ -0,0 +1,25 @@
+'''
+Given an array of integers nums and an integer target, return indices of the 2 numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice
+
+You can return the answer in any order
+
+Example 1:
+input: nums = [2, 7, 11, 15], target = 9
+output: [0, 1]
+Explanation: because nums[0] + nums[1] == 9, we return [0, 1]
+
+Example 2:
+
+input: nums = [3, 2, 4], target = 6
+output: [1, 2]
+
+Example 3:
+input: nums = [3, 3], target = 6
+Output: [0, 1]
+
+'''
+
+def twoSum(nums, target):
+    return None
